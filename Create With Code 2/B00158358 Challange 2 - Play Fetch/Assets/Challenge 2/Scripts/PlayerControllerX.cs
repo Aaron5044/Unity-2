15611170d3dd14a4145287860beb57e93ee761c6
Aaron5044 <B00158358@mytudublin.ie>
2023-10-06 13:07:58 +0100

    extra files

---
+++ Create With Code 2/B00158358 Challange 2 - Play Fetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float slowDown = 20.0f;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        slowDown -= 0.1f;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            slowDown = 20.0f;
+        }
+         
+        
+        
+    }
+}
